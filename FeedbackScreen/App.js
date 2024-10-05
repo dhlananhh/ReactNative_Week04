7c0d36037b0953d5b92353a1d97d8a81beb435da
dhlananhh <21087481.anh@student.iuh.edu.vn>
2024-10-05 21:03:01 +0700

    update

--- FeedbackScreen/App.js
+++ FeedbackScreen/App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
-export default function App() {
+const FeedbackScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Image
+          source={ require('./assets/usb_icon.png') }
+          style={styles.usbIcon}
+        />
+        <Text style={styles.title}>
+          USB Bluetooth Music Receiver HJX-001 - Biến loa thường thành loa Bluetooth
+        </Text>
+      </View>
+
+      <View style={styles.ratingContainer}>
+        <Text style={styles.ratingText}>
+          Cực kỳ hài lòng
+        </Text>
+        <View style={styles.starsContainer}>
+          <Text style={styles.ratingStars}>
+            ★★★★★
+          </Text>
+        </View>
+      </View>
+
+      <TouchableOpacity style={styles.imageUploadButton}>
+        <FontAwesome name="camera" size={30} color="black" />
+        <Text style={styles.imageUploadText}>
+          Thêm hình ảnh
+        </Text>
+      </TouchableOpacity>
+
+      <TextInput
+        style={styles.feedbackInput}
+        placeholder="Hãy chia sẻ những điều mà bạn thích về sản phẩm"
+        placeholderTextColor="#C4C4C4"
+        multiline
+      />
+
+      <TouchableOpacity style={submitButton}>
+        <Text style={styles.submitButtonText}>
+          Gửi
+        </Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
+
+export default FeedbackScreen;
